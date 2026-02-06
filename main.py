8915004114d89a15a2d4c9631c55e417580bf474
osawese-ninja27 <dadamoyosore92@gmail.com>
2026-02-06 13:41:20 +0100

    initial commit

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/home")
-def read_root():
-    return {"hello": "world"}
+DATABASE_URL = "sqlite:///./test.db"
+
+engine = create_engine(DATABASE_URL, connect_args*{"check_same_thread": False})
+SessionLocal =sessionmaker(autocommit= False, autoflush= False, bind=engine)
+Base = declarative_base()
+class user(Base):
+    __tablename__ ="users"
+
+    id= Column(Integer, primary_key= True, index =True)
+    name =Column(String, Index = True)
+    email = Column(String, unique =True, Index =True)
+Base.metadata.create_all(bind= engine)
